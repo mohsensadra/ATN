6ca6bba21e03f181789f7a930a6ae56fafa3338e
Mohsen Sadra <45586184+mohsensadra@users.noreply.github.com>
2024-04-16 13:47:52 +0330

    Update Response simulator.py

--- Response simulator.py
+++ Response simulator.py
@@ -14,17 +14,17 @@ cylinder_mass = 1.014 # kg
 rotation_speed = 1000 # rpm
 
 # Unbalance mass parameters
-m1 = random.uniform(0.0, 0.02) # kg
-m2 = random.uniform(0.0, 0.02) # kg
-r1 = random.uniform(0.0, 0.01) # m
-r2 = random.uniform(0.0, 0.01) # m
-theta1 = random.uniform(0.0, 360.) # degrees
-theta2 = random.uniform(0.0, 360.) # degrees
+m1 = 0.01 #random.uniform(0.0, 0.02) # kg
+m2 = 0.02 #random.uniform(0.0, 0.02) # kg
+r1 = 0.005 #random.uniform(0.0, 0.01) # m
+r2 = 0.007 #random.uniform(0.0, 0.01) # m
+theta1 = 50. #random.uniform(0.0, 360.) # degrees
+theta2 = 120. #random.uniform(0.0, 360.) # degrees
 
 # Convert theta1 and theta2 to radians
 theta1_rad = np.deg2rad(theta1)
 theta2_rad = np.deg2rad(theta2)
-print ('theta1:',theta1,' Phase difference:',theta2-theta1, ' deg')
+print ('Alpha:',theta1,' Phase difference:',theta2-theta1, ' deg')
 # Sensor positions
 sensor1_distance = 0.308 # m
 sensor2_distance = sensor1_distance + 0.19 # m
@@ -49,13 +49,13 @@ unbalance_force_2 = m2 * r2 * omega ** 2 * np.cos(omega * t + theta2_rad)
 net_unbalance_force = unbalance_force_1 + unbalance_force_2
 
 # Calculate acceleration at the sensors
-amplitude1 = (cylinder_mass * net_unbalance_force) / (cylinder_mass * sensor1_distance)
-amplitude2 = (cylinder_mass * net_unbalance_force) / (cylinder_mass * sensor2_distance)
+amplitude1 = np.max(net_unbalance_force / sensor1_distance)
+amplitude2 = np.max(net_unbalance_force / sensor2_distance)
 
 # Generate sinusoidal signals with different amplitudes and phase shifts
 signal1 = amplitude1 * np.sin(omega * t + theta1_rad)
 signal2 = amplitude2 * np.sin(omega * t + theta2_rad)
-main_signal=np.sin(omega*t)
+#main_signal=amplitude1 *np.sin(omega*t)
 # Compute phase difference between signals
 complex_signal1 = signal1 + 1j * np.zeros_like(signal1)
 complex_signal2 = signal2 + 1j * np.zeros_like(signal2)
@@ -73,7 +73,7 @@ df.to_csv('vibration_data.csv', index=False)
 plt.figure(figsize=(10, 6))
 plt.plot(t, signal1, label="Signal 1")
 plt.plot(t, signal2, label="Signal 2")
-plt.plot(t, main_signal, label="Main Signal")
+#plt.plot(t, main_signal, label="Main Signal")
 plt.title("Simulated Acceleration Signals")
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
